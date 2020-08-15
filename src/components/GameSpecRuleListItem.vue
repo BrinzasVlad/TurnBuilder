a8ba2813c3a097f8992ec7f38d4fdd2a1504d683
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-15 21:14:28 +0300

    Created Vue element for specifying a rule

---
+++ src/components/GameSpecRuleListItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="game-spec-rule-list-item">
+      <span>When </span>
+      <v-select
+        class="rule-trigger"
+        :clearable="false"
+        :options="TriggerTypes.allValues"
+        :value="rule.trigger"
+        @input="(newTrigger) => triggerChange(newTrigger)"
+      />
+      <span>, </span>
+      <game-spec-rule-effect
+        :effect="rule.effect"
+      />
+    </div>
+</template>
+
+<script>
+import TriggerTypes from '@/utils/TriggerTypes'
+import vSelect from 'vue-select'
+import GameSpecRuleEffect from './GameSpecRuleEffect.vue'
+
+export default {
+  name: 'GameSpecRuleListItem',
+  components: {
+    vSelect,
+    GameSpecRuleEffect
+  },
+  props: {
+    rule: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    console: () => console,
+    TriggerTypes: () => TriggerTypes
+  },
+  methods: {
+    triggerChange (newTrigger) {
+      this.$emit('rule-trigger-change', newTrigger)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.game-spec-rule-list-item {
+  display: flex;
+  flex-direction: row;
+}
+span {
+  white-space: pre-wrap;
+}
+</style>
