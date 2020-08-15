0985eee2f55f2999374f6de0b86ca8577fdbf429
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-15 23:01:41 +0300

    Created Vue element for specifying an effect.
    Also created a specific Vue element for the effect of setting the value
    of an attribute.

---
+++ src/components/GameSpecRuleEffect.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="game-spec-player-count">
+      <v-select
+        class="rule-effect"
+        :clearable="false"
+        :options="effectsList"
+        label="text"
+      >
+        <template #selected-option="{ component }" class="vs__selected">
+          <component :is="component" />
+        </template>
+      </v-select>
+    </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+import GameSpecRuleEffectSetAttribute from './GameSpecRuleEffectSetAttribute.vue'
+
+export default {
+  name: 'GameSpecRuleEffect',
+  props: {
+    effect: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    vSelect,
+    GameSpecRuleEffectSetAttribute
+  },
+  computed: {
+    console: () => console,
+    effectsList: function () {
+      const effectsToAdd = [
+        GameSpecRuleEffectSetAttribute
+      ]
+      return effectsToAdd.map(effect => {
+        return { component: effect.name, text: effect.computed.text() }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+span {
+  white-space: pre-wrap;
+}
+</style>
---
+++ src/components/GameSpecRuleEffectSetAttribute.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="game-spec-rule-effect-set-attribute">
+        <span>{{textChunks[0]}}</span>
+        <game-spec-selector-attribute />
+        <span>{{textChunks[1]}}</span>
+        <!-- TODO: value selector -->
+    </div>
+</template>
+
+<script>
+import GameSpecSelectorAttribute from './GameSpecSelectorAttribute.vue'
+
+export default {
+  name: 'GameSpecRuleEffectSetAttribute',
+  components: {
+    GameSpecSelectorAttribute
+  },
+  computed: {
+    console: () => console,
+    text: () => 'set the value of ... to ...',
+    textChunks: function () { return this.text.split('...') }
+  }
+}
+</script>
+
+<style scoped>
+.game-spec-rule-effect-set-attribute {
+  display: flex;
+  flex-direction: row;
+}
+span {
+  white-space: pre-wrap;
+}
+</style>
