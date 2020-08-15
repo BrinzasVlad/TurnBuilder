9e005c9d8dc7f22e1cdd40d2c1b7e200ca78bd5d
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-15 21:08:29 +0300

    Created Rule object. Created enumerated type for possible rule triggers.

---
+++ src/utils/Rule.js
@@ -0,0 +1,24 @@
+import TriggerTypes from '@/utils/TriggerTypes'
+
+export default class GameRule {
+  constructor(trigger, effect) {
+    this._trigger = trigger
+    this._effect = effect
+  }
+
+  set trigger(value) {
+    if(!TriggerTypes.allValues().includes(value)) {
+      throw new Error('not a valid trigger') // TODO: are errors thrown correctly?
+    }
+    this._trigger = value
+  }
+
+  get trigger() { return this._trigger }
+
+  set effect(value) {
+    // TODO: check that what we get is actually an effect
+    this._effect = value
+  }
+
+  get effect() { return this._effect }
+}
---
+++ src/utils/TriggerTypes.js
@@ -0,0 +1,5 @@
+import { addAllValuesProperty } from '@/utils/EnumUtils.js'
+
+export default Object.freeze(addAllValuesProperty({
+  GAME_START: 'the game starts'
+}))
