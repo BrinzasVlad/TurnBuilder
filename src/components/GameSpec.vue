f801ead7f5665e8d852adcde51226da6e5f0d515
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-17 20:28:01 +0300

    Added Vue component for displaying and adding rules.

--- src/components/GameSpec.vue
+++ src/components/GameSpec.vue
@@ -1,24 +1,28 @@
-<!-- TODO: should contain components for specifying player / grid / state / piece / rule matters -->
-
 <template>
     <div class="game-spec">
       <game-spec-player />
       <game-spec-grid />
+      <game-spec-game-attr />
       <game-spec-piece-list />
+      <game-spec-rule-list />
     </div>
 </template>
 
 <script>
 import GameSpecPlayer from './GameSpecPlayer.vue'
 import GameSpecGrid from './GameSpecGrid.vue'
+import GameSpecGameAttr from './GameSpecGameAttr.vue'
 import GameSpecPieceList from './GameSpecPieceList.vue'
+import GameSpecRuleList from './GameSpecRuleList.vue'
 
 export default {
   name: 'GameSpec',
   components: {
     GameSpecPlayer,
     GameSpecGrid,
-    GameSpecPieceList
+    GameSpecGameAttr,
+    GameSpecPieceList,
+    GameSpecRuleList
   },
   computed: {
     console: () => console
---
+++ src/components/GameSpecRuleList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="game-spec-rule-list">
+      <h2>Game rules:</h2>
+      <ul>
+        <li
+          is="game-spec-rule-list-item"
+          v-for="ruleWithKey in rulesWithKeys"
+          :key="ruleWithKey.key"
+          :rule="ruleWithKey.rule"
+        />
+        <li class="add-rule-entry">
+          <button @click="addRule">Add new rule...</button>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+import GameSpecRuleListItem from './GameSpecRuleListItem.vue'
+
+let keyCounter = 1
+
+export default {
+  name: 'GameSpecRuleList',
+  components: {
+    GameSpecRuleListItem
+  },
+  computed: {
+    console: () => console,
+    rulesWithKeys () {
+      return this.$store.state.gameSpec.rules.map((rule) => {
+        return { key: keyCounter++, rule: rule }
+      })
+    }
+  },
+  methods: {
+    addRule () {
+      this.$store.commit('addRule')
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  text-align: left;
+}
+.add-rule-entry {
+  text-align: left;
+}
+</style>
