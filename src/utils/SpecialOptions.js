487f112dd254a5d0db72347305d163b41281ec18
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-23 01:02:22 +0300

    Created SpecialOptions enum type for special selector / effect options
    Also added a method to Trigger types that associates to each trigger the
    appropriate special options. For instance, the TILE_SELECTED trigger
    enables the presence of tile selectors such as 'tile that was selected'.

---
+++ src/utils/SpecialOptions.js
@@ -0,0 +1,13 @@
+import { addAllValuesProperty } from '@/utils/EnumUtils.js'
+
+/**
+ * An enumeration that contains the possible types
+ * of special options that are unlocked only for
+ * certain types of triggers (for example, the
+ * 'piece selected' option is only available if
+ * the trigger was someone selecting a piece in the
+ * first place)
+ */
+export default Object.freeze(addAllValuesProperty({
+  TILE_SELECTED: 'tile selected'
+}))
--- src/utils/TriggerTypes.js
+++ src/utils/TriggerTypes.js
@@ -1,6 +1,23 @@
 import { addAllValuesProperty } from '@/utils/EnumUtils.js'
+import SpecialOptions from '@/utils/SpecialOptions.js'
 
+/**
+ * An enumeration that contains the possible types of
+ * triggers that a rule can have
+ */
 export default Object.freeze(addAllValuesProperty({
   GAME_START: 'the game starts',
-  TILE_SELECTED: 'the current player selects a tile'
+  TILE_SELECTED: 'the current player selects a tile',
+  /**
+   * Return the special options associated to the given trigger
+   * (as an array), as per SpecialOptions
+   * @param {TriggerType} triggerType The type of trigger
+   * for which one wants to find the special options
+   */
+  specialOptions (triggerType) {
+    switch (triggerType) {
+      case this.TILE_SELECTED: return [SpecialOptions.TILE_SELECTED]
+      default: return []
+    }
+  }
 }))
