59eac68ff9c8e11fe9c0053104b71e592939754b
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-29 16:50:51 +0300

    Added reminder to check how licensing should be managed in the app.
    Since we are using Vue Select, which is licensed by Jeff Sagal under the
    MIT License, it is possible that the usage of his component should be
    accompanied by some kind of notice indicating such. Since I do not know
    currently what a such notice should include, I have attached a FIXME so the
    issue is not forgotten.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'vue-select/dist/vue-select.css'
 
 Vue.config.productionTip = false
 
+// FIXME: should we mention here that we are using Vue Select under the permissions of its MIT License?
 Vue.component('v-select', vSelect)
 
 new Vue({
