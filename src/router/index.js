377e2973891d46f79a1b6230c5583ecf4f6afbc5
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-08-29 20:39:15 +0300

    Removed About from the navbar, Home now redirects, deleted Home page.
    The Home page has been deleted entirely (mostly because it did not provide
    anything of value to the project) and the navbar link now redirects to the
    Game Specification page.
    The About page has had its router path and navbar link removed for now, but
    the page itself has been kept for future re-introduction once it provides
    useful content.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import GameSpecification from '../views/GameSpecification.vue'
 import GamePlaytest from '../views/GamePlaytest.vue'
 import TestView from '../views/TestView.vue'
@@ -9,22 +8,10 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/specify',
     name: 'Game Specification',
-    component: GameSpecification
+    component: GameSpecification,
+    alias: '/'
   },
   {
     path: '/playtest',
