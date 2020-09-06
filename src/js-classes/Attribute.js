e3f72f4e920ad2c86843b4074847a35694db3371
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-09-06 22:19:25 +0300

    Overloaded constructor of Attribute to also allow clone-constructing.
    This is because in a number of instances it is desirable to clone an
    Attribute object, with no good options available. Since Attribute is the
    class best informed to manage this action, responsibility has been
    allocated to Attribute.

--- src/js-classes/Attribute.js
+++ src/js-classes/Attribute.js
@@ -1,6 +1,30 @@
 import AttributeTypes from '@/utils/AttributeTypes'
 export default class Attribute {
-  constructor (name, type, editable = true) {
+  // TODO: this is actually an overloaded constructor, but JSDoc does not seem to support that
+  /**
+   * Creates a new Attribute object.
+   *
+   * If called with a single Attribute as argument, it creates a clone of that Attribute.
+   * @param {Attribute} attribute The Attribute to clone.
+   *
+   * If called with multiple arguments, it instead creates a new attribute from the following (in order):
+   * @param {string} name The unique name of the Attribute
+   * @param {AttributeTypes} type The type of value referenced by the Attribute
+   * @param {boolean} [editable = true] Whether the user can change the attribute (default true)
+   */
+  constructor (...args) {
+    let name, type, editable
+    if (args[0] instanceof Attribute) {
+      name = args[0]._name
+      type = args[0]._type
+      editable = args[0]._editable
+    } else {
+      name = args[0]
+      type = args[1]
+      if (undefined === args[2]) editable = true
+      else editable = args[2]
+    }
+
     this._name = name
     this._type = type
     this._editable = editable
