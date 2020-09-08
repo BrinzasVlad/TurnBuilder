099d89871b4e5278107eea50b5b43383188a2532
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-09-08 08:32:09 +0300

    Fixed JS class for 'the piece that was selected' using wrong method name.
    The class was presenting method getType, but the parent, SelectorValue, had
    switched to presenting getValueType.

--- src/js-classes/SelectorValueSelectedPiece.js
+++ src/js-classes/SelectorValueSelectedPiece.js
@@ -2,7 +2,7 @@ import AttributeTypes from '@/utils/AttributeTypes'
 import SelectorValue from './SelectorValue'
 
 export default class SelectorValueSelectedPiece extends SelectorValue {
-  getType () {
+  getValueType () {
     return AttributeTypes.PIECE
   }
 
