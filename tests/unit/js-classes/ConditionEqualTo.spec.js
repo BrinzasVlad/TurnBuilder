f20cd61a35785849dcd05d65f39c02ff1929e9f5
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-09-08 04:52:48 +0300

    Updated unit test to use new game specification utils.

--- tests/unit/js-classes/ConditionEqualTo.spec.js
+++ tests/unit/js-classes/ConditionEqualTo.spec.js
@@ -1,35 +1,27 @@
 import ConditionEqualTo from '@/js-classes/ConditionEqualTo'
-import SelectorValue from '@/js-classes/ConditionEqualTo'
-import AttributeTypes from '@/utils/AttributeTypes'
 import { createPlayer, createTile, createPiece } from '../../utils/GamePlayObjectUtils'
+import { createValueSelector } from '../../utils/GameSpecObjectUtils'
 
-function valueSelector (valueToReturn, valueType) {
-  class TestSelector extends SelectorValue {
-    getValueType () { return valueType }
-    getValue (_gameState, _triggerArgs) { return (_finalGameState) => valueToReturn }
-  }
-  return new TestSelector()
-}
 describe('ConditionEqualTo.js', () => {
   describe('Number Comparison', () => {
     it('should assess 3 and 3 as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(3, AttributeTypes.NUMBER),
-        valueSelector(3, AttributeTypes.NUMBER)
+        createValueSelector(3),
+        createValueSelector(3)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess 0 and 0 as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(0, AttributeTypes.NUMBER),
-        valueSelector(0, AttributeTypes.NUMBER)
+        createValueSelector(0),
+        createValueSelector(0)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess 4 and -4 as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(4, AttributeTypes.NUMBER),
-        valueSelector(-4, AttributeTypes.NUMBER)
+        createValueSelector(4),
+        createValueSelector(-4)
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
@@ -38,29 +30,29 @@ describe('ConditionEqualTo.js', () => {
   describe('Text Comparison', () => {
     it('should assess "Wolf" and "Wolf" as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector('Wolf', AttributeTypes.TEXT),
-        valueSelector('Wolf', AttributeTypes.TEXT)
+        createValueSelector('Wolf'),
+        createValueSelector('Wolf')
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess "Wolf" and "Sheep" as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector('Wolf', AttributeTypes.TEXT),
-        valueSelector('Sheep', AttributeTypes.TEXT)
+        createValueSelector('Wolf'),
+        createValueSelector('Sheep')
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
     it('should assess "Wolf" and "Wolf " as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector('Wolf', AttributeTypes.TEXT),
-        valueSelector('Wolf ', AttributeTypes.TEXT)
+        createValueSelector('Wolf'),
+        createValueSelector('Wolf ')
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
     it('should assess "Wolf" and "wolf" as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector('Wolf', AttributeTypes.TEXT),
-        valueSelector('wolf', AttributeTypes.TEXT)
+        createValueSelector('Wolf'),
+        createValueSelector('wolf')
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
@@ -70,22 +62,22 @@ describe('ConditionEqualTo.js', () => {
     it('should assess Player 1 and Player 1 (same object reference) as equal', () => {
       const playerOne = createPlayer(1)
       const condition = new ConditionEqualTo(
-        valueSelector(playerOne, AttributeTypes.PLAYER),
-        valueSelector(playerOne, AttributeTypes.PLAYER)
+        createValueSelector(playerOne),
+        createValueSelector(playerOne)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Player 1 and Player 1 (different object reference) as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createPlayer(1), AttributeTypes.PLAYER),
-        valueSelector(createPlayer(1), AttributeTypes.PLAYER)
+        createValueSelector(createPlayer(1)),
+        createValueSelector(createPlayer(1))
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Player 1 and Player 2 as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createPlayer(1), AttributeTypes.PLAYER),
-        valueSelector(createPlayer(2), AttributeTypes.PLAYER)
+        createValueSelector(createPlayer(1)),
+        createValueSelector(createPlayer(2))
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
@@ -95,51 +87,51 @@ describe('ConditionEqualTo.js', () => {
     it('should assess Tile(1, 1) and Tile(1, 1) (same object reference, no content) as equal', () => {
       const tile = createTile(1, 1)
       const condition = new ConditionEqualTo(
-        valueSelector(tile, AttributeTypes.TILE),
-        valueSelector(tile, AttributeTypes.TILE)
+        createValueSelector(tile),
+        createValueSelector(tile)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Tile(1, 1) and Tile(1, 1) (same object reference, with content) as equal', () => {
       const tile = createTile(1, 1, createPiece())
       const condition = new ConditionEqualTo(
-        valueSelector(tile, AttributeTypes.TILE),
-        valueSelector(tile, AttributeTypes.TILE)
+        createValueSelector(tile),
+        createValueSelector(tile)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Tile(1, 1) and Tile(1, 1) (different object reference, no content) as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createTile(1, 1), AttributeTypes.TILE),
-        valueSelector(createTile(1, 1), AttributeTypes.TILE)
+        createValueSelector(createTile(1, 1)),
+        createValueSelector(createTile(1, 1))
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Tile(1, 1) and Tile(1, 1) (different object reference, same content) as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createTile(1, 1, createPiece()), AttributeTypes.TILE),
-        valueSelector(createTile(1, 1, createPiece()), AttributeTypes.TILE)
+        createValueSelector(createTile(1, 1, createPiece())),
+        createValueSelector(createTile(1, 1, createPiece()))
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Tile(1, 1) and Tile(1, 1) (different object reference, different content) as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createTile(1, 1, createPiece(1, "Dummy One")), AttributeTypes.TILE),
-        valueSelector(createTile(1, 1, createPiece(2, "Dummy Two")), AttributeTypes.TILE)
+        createValueSelector(createTile(1, 1, createPiece(1, "Dummy One"))),
+        createValueSelector(createTile(1, 1, createPiece(2, "Dummy Two")))
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Tile(1, 1) and Tile(1, 2) as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createTile(1, 1), AttributeTypes.TILE),
-        valueSelector(createTile(1, 2), AttributeTypes.TILE)
+        createValueSelector(createTile(1, 1)),
+        createValueSelector(createTile(1, 2))
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
     it('should assess Tile(1, 2) and Tile(2, 2) as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createTile(1, 2), AttributeTypes.TILE),
-        valueSelector(createTile(2, 2), AttributeTypes.TILE)
+        createValueSelector(createTile(1, 2)),
+        createValueSelector(createTile(2, 2))
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
@@ -149,29 +141,29 @@ describe('ConditionEqualTo.js', () => {
     it('should assess Piece 1 and Piece 1 (same object reference) as equal', () => {
       const pieceOne = createPiece(1)
       const condition = new ConditionEqualTo(
-        valueSelector(pieceOne, AttributeTypes.PIECE),
-        valueSelector(pieceOne, AttributeTypes.PIECE)
+        createValueSelector(pieceOne),
+        createValueSelector(pieceOne)
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Piece 1 and Piece 1 (different object reference) as equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createPiece(1), AttributeTypes.PIECE),
-        valueSelector(createPiece(1), AttributeTypes.PIECE)
+        createValueSelector(createPiece(1)),
+        createValueSelector(createPiece(1))
       )
       expect(condition.isTrue({}, {})).toBe(true)
     })
     it('should assess Piece 2 and Piece 3 (same type) as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createPiece(2, 'DummyType'), AttributeTypes.PIECE),
-        valueSelector(createPiece(3, 'DummyType'), AttributeTypes.PIECE)
+        createValueSelector(createPiece(2, 'DummyType')),
+        createValueSelector(createPiece(3, 'DummyType'))
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
     it('should assess Piece 2 and Piece 3 (different type) as not equal', () => {
       const condition = new ConditionEqualTo(
-        valueSelector(createPiece(2, 'DummyTypeOne'), AttributeTypes.PIECE),
-        valueSelector(createPiece(3, 'DummyTypeTwo'), AttributeTypes.PIECE)
+        createValueSelector(createPiece(2, 'DummyTypeOne')),
+        createValueSelector(createPiece(3, 'DummyTypeTwo'))
       )
       expect(condition.isTrue({}, {})).toBe(false)
     })
