5fe2c0104f76093f8b3c2e0489824689b7d31c49
Vlad Brinzas <vlad.brinzas@gmail.com>
2020-09-08 08:35:52 +0300

    Fixed 'the tile that was selected' to expose the correct method name.

--- src/js-classes/SelectorValueSelectedTile.js
+++ src/js-classes/SelectorValueSelectedTile.js
@@ -2,7 +2,7 @@ import AttributeTypes from '@/utils/AttributeTypes'
 import SelectorValue from './SelectorValue'
 
 export default class SelectorValueSelectedTile extends SelectorValue {
-  getType () {
+  getValueType () {
     return AttributeTypes.TILE
   }
 
